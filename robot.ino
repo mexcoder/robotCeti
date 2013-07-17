c14600a57e70087cf1e3a66d3e9bf79b6fc18cd2
Juan Antonio Orozco Bianchi <iam@mexcoder.com>
2013-07-16 19:16:34 -0500

    bare bones
    Signed-off-by: Juan Antonio Orozco Bianchi <iam@mexcoder.com>

---
+++ robot.ino
@@ -0,0 +1,38 @@
+
+const int selectorColor = 8;
+const int pwm = 9;
+const int motorDer0 = 10;
+const int motorDer1 = 11;
+const int motorIzq0 = 12;
+const int motorIzq1 = 13;
+const int selectorPrograma = 6;
+const int siguienteEstado = 7;
+const int selectorColor = 8;
+const int s0 = A0;
+const int s1 = A1;
+const int s2 = A2;
+const int s3 = A3;
+const int s4 = A4;
+
+/**
+ * LCD RS pin to digital pin 4
+ * LCD Enable pin to digital pin 5
+ * LCD D4 pin to digital pin 0
+ * LCD D5 pin to digital pin 1
+ * LCD D6 pin to digital pin 2
+ * LCD D7 pin to digital pin 3
+**/
+
+#include <LiquidCrystal.h>
+
+LiquidCrystal lcd(4, 5, 0, 1, 2, 3);
+
+
+void setup() {
+  // put your setup code here, to run once
+}
+
+void loop() {
+  // put your main code here, to run repeatedly: 
+  
+}
